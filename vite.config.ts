41c67afc527bf6f10b99afcb637ad098d0517357
Larisa Parasca <parasca.larisa31@gmail.com>
2026-03-23 10:44:36 +0200

    feat: added project files

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/DigiCode/',
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    target: 'ES2020',
+  },
+});
